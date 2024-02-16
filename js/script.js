b7d65abae397205a9ea51f085d78f5ae23607787
MohammadTanvir881 <tanvirrashid881@gmail.com>
2024-02-16 06:48:44 -0500

    modification done

--- js/script.js
+++ js/script.js
@@ -19,7 +19,36 @@ function KeyboardButtonPressed(event) {
         const finalScore = setValueOfScore('current-score');
         const score = document.getElementById('current-score');
         score.innerText = finalScore;
+        const scoreRating = document.getElementById('rating-score');
+        // console.log(scoreRating)
+        
+        
         setValueAfterEndTheGame('game-score', finalScore)
+    // if(finalScore > 20 && finalScore <= 50){
+    //     scoreRating.innerText = 'You Are Noob At Typing';
+    // }
+    // else if( finalScore <= 100){
+    //     scoreRating.innerText = 'You Are Doing Good';
+    // }
+    // else if( finalScore <= 150){
+    //     scoreRating.innerText = 'You Are On A right Track . Keep it up';
+    // }
+    // else if( finalScore <= 200){
+    //     scoreRating.innerText = 'You Are Doing Excellent Brohh';
+    // }
+    // else if( finalScore <=300){
+    //     scoreRating.innerText = 'Dammm Sigma Exixts';
+    // }
+    // else if( finalScore <= 500){
+    //     scoreRating.innerText = 'Contact To Elon Mask';
+    // }
+    // else if( finalScore <= 2000){
+    //     scoreRating.innerText = 'Are You Gonna Break My Game???';
+    // }
+    // else if( finalScore <= 5000){
+    //     scoreRating.innerText = 'No Comments For You . You Are In Another Level';
+    // }
+        
         continueGame();
 
     }
@@ -55,5 +84,10 @@ function play() {
     hideElementById('total-score');
     showElementById('play-ground')
     setValueAfterEndTheGame('current-life', 5);
-    setValueAfterEndTheGame('current-score', 0)
+    setValueAfterEndTheGame('current-score', 0);
+    const scoreRating = document.getElementById('rating-score');
+    scoreRating.innerText = 'You Are Noob At Typing';
+    console.log(scoreRating)
+    
+    
 }
